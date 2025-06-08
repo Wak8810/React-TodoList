d110e365aebd0e16dc0bbfd90a2bcd5f5154c5ce
Wak8810 <231205192@ccmailg.meijo-u.ac.jp>
2025-06-08 18:03:32 +0900

    フォルダ構成の変更

--- README.md
+++
@@ -1,4 +0,0 @@
-# 概要
-自作したTodoリストアプリ。GithubPagesにてデプロイ済みであるため、閲覧・使用可能である。
-# 使用技術
-React + TypeScript + Tailwind CSS
--- src/types/todo.tsx
+++
@@ -1,6 +0,0 @@
-export type todo = {
-    index: number;
-    text: string;
-    priority: number;
-    checked: boolean;
-};
